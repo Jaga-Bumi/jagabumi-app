a92ae74a9aa930d55b3bfc9afe57c171c697bfe9
Aaron <aaronjeremy2004@gmail.com>
2025-12-04 15:56:09 +0700

    added prize routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrganizationController;
+use App\Http\Controllers\PrizeController;
 use App\Http\Controllers\QuestController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -33,30 +34,35 @@ Route::get('home', function(){
 });
 
 Route::prefix('quests')->name('quests.')->group(function(){
-    Route::get('', [QuestController::class, 'getAll'])->name('all');
-    Route::get('{id}', [QuestController::class, 'getOne'])->name('one');
+    Route::get('', [QuestController::class, 'readAll'])->name('all');
+    Route::get('{id}', [QuestController::class, 'readOne'])->name('one');
     Route::post('create', [QuestController::class, 'create'])->name('create');
     Route::put('update/{id}', [QuestController::class, 'update'])->name('update');
     Route::delete('destroy/{id}', [QuestController::class, 'destroy'])->name('destroy');
 });
 
 Route::prefix('articles')->name('articles.')->group(function(){
-    Route::get('', [ArticleController::class, 'getAll'])->name('all');
-    Route::get('{id}', [ArticleController::class, 'getOne'])->name('one');
+    Route::get('', [ArticleController::class, 'readAll'])->name('all');
+    Route::get('{id}', [ArticleController::class, 'readOne'])->name('one');
     Route::post('create', [ArticleController::class, 'create'])->name('create');
     Route::put('update/{id}', [ArticleController::class, 'update'])->name('update');
     Route::delete('destroy/{id}', [ArticleController::class, 'destroy'])->name('destroy');
 });
 
-Route::prefix('organization')->name('org.')->group(function(){
-    Route::get('', [OrganizationController::class, 'getAll'])->name('all');
-    Route::get('{id}', [OrganizationController::class, 'getOne'])->name('one');
-    Route::post('create', [OrganizationController::class, 'create'])->name('create');
+Route::prefix('organizations')->name('org.')->group(function(){
+    Route::get('', [OrganizationController::class, 'readAll'])->name('all');
+    Route::get('{id}', [OrganizationController::class, 'readOne'])->name('one');
     Route::put('update/{id}', [OrganizationController::class, 'update'])->name('update');
     Route::delete('destroy/{id}', [OrganizationController::class, 'destroy'])->name('destroy');
 });
 
-
+Route::prefix('prizes')->name('prizes.')->group(function(){
+    Route::get('', [PrizeController::class, 'readAll'])->name('all');
+    Route::get('{id}', [PrizeController::class, 'readOne'])->name('one');
+    Route::post('create', [PrizeController::class, 'create'])->name('create');
+    Route::put('update/{id}', [PrizeController::class, 'update'])->name('update');
+    Route::delete('destroy/{id}', [PrizeController::class, 'destroy'])->name('destroy');
+});
 
 
 
