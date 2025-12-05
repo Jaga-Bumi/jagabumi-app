3fe298b68a8f3c182eb2b86ce96fe7a582ce0ed7
Aaron <aaronjeremy2004@gmail.com>
2025-12-05 10:30:53 +0700

    a

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,8 +20,7 @@ return new class extends Migration
             $table->string('profile_pic_url')->nullable();
             $table->string('auth_provider')->default('google');
             $table->string('password')->nullable();
-            $table->enum('role', ['user, admin'])->default('user');
-            $table->foreignId('org_id')->nullable()->references('id')->on('organizations')->nullOnDelete();
+            $table->enum('role', ['user','admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
