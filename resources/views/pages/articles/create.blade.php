b880f0262d34e43517da929712145030b6d259fd
Aaron <aaronjeremy2004@gmail.com>
2025-12-17 19:48:39 +0700

    feat: changed articles pages to indo

--- resources/views/pages/articles/create.blade.php
+++ resources/views/pages/articles/create.blade.php
@@ -70,7 +70,7 @@
             <div class="flex justify-end">
                 <button type="submit"
                         class="inline-flex justify-center py-2 px-6 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-150 ease-in-out">
-                    Simpan Artikel
+                    Buat Artikel
                 </button>
             </div>
 
