cd13cd204f5c99b2d7b9f27bbc9cadd8d47dbc20
Aaron <aaronjeremy2004@gmail.com>
2025-12-18 22:29:12 +0700

    feat: changed components to indo

--- resources/views/components/organization/sidebar.blade.php
+++ resources/views/components/organization/sidebar.blade.php
@@ -70,7 +70,7 @@
       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
       </svg>
-      <span x-show="!collapsed">Organization Profile</span>
+      <span x-show="!collapsed">Profil Organisasi</span>
     </a>
 
     {{-- Member Management (CREATOR only) --}}
@@ -81,7 +81,7 @@
       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
       </svg>
-      <span x-show="!collapsed">Member Management</span>
+      <span x-show="!collapsed">Manajemen Anggota</span>
     </a>
     @endif
 
@@ -92,7 +92,7 @@
       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
       </svg>
-      <span x-show="!collapsed">Quest Management</span>
+      <span x-show="!collapsed">Manajemen Quest</span>
     </a>
 
     {{-- Articles Management --}}
@@ -103,7 +103,7 @@
             {{-- Icon for Articles (e.g., a document or book) --}}
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5s3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18s-3.332.477-4.5 1.253" />
         </svg>
-        <span x-show="!collapsed">Articles Management</span>
+        <span x-show="!collapsed">Manajemen Artikel</span>
     </a>
 
     {{-- Submission Review --}}
@@ -123,7 +123,7 @@
       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
       </svg>
-      <span x-show="!collapsed">Prize Distribution</span>
+      <span x-show="!collapsed">Pemberian Hadiah</span>
     </a>
   </nav>
 
@@ -135,7 +135,7 @@
       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
-      <span x-show="!collapsed">Back to Main Site</span>
+      <span x-show="!collapsed">Balik ke Main Site</span>
     </a>
   </div>
 </aside>
