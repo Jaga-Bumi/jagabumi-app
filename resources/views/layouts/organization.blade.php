2a31e598fae0fb4459524dadc2ef0cd3e0932951
Ryu Orlando Tamin <ryutamin05@gmail.com>
2026-02-01 16:11:18 +0700

    edit icon

--- resources/views/layouts/organization.blade.php
+++ resources/views/layouts/organization.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ $title ?? '' ? ($title ?? '') . ' - ' : '' }}Organization Dashboard - JagaBumi</title>
-  <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+  <link rel="icon" href="{{ asset('logo.svg') }}" type="image/svg+xml">
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
